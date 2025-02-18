9c53acc9e6cd023557ca83db915d073841456b77
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2025-02-18 16:59:19 +0700

    Update Limit-Ohio64.sh

--- Limit-Ohio64.sh
+++ Limit-Ohio64.sh
@@ -6,7 +6,7 @@ REGIONS=("us-east-1" "us-west-2" "us-east-2")
 # Cấu hình chung
 NEW_QUOTA_VALUE=64   # Giá trị hạn mức mới bạn muốn
 SERVICE_CODE="ec2"   # Mã dịch vụ EC2
-QUOTA_CODE="L-1216C47A"  # Mã hạn mức cho Spot Instances (vCPU)
+QUOTA_CODE="L-1216C47A"  # Mã hạn mức cho Instances (vCPU)
 
 # Lặp qua từng khu vực và gửi yêu cầu tăng hạn mức
 for REGION in "${REGIONS[@]}"; do
@@ -21,7 +21,7 @@ for REGION in "${REGIONS[@]}"; do
 
     # Kiểm tra trạng thái yêu cầu
     if [ $? -eq 0 ]; then
-        echo "Yêu cầu tăng hạn mức CPU cho Spot Instances đã được gửi thành công tại khu vực $REGION."
+        echo "Yêu cầu tăng hạn mức CPU cho Instances đã được gửi thành công tại khu vực $REGION."
     else
         echo "Có lỗi xảy ra khi gửi yêu cầu tại khu vực $REGION. Vui lòng kiểm tra lại cấu hình và quyền IAM."
     fi
